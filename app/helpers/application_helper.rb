2bd4ca1fc90d31add41614c8c847feedf47bb254
Dom Leone <ohheydom@gmail.com>
2014-04-22 00:37:28 -0400

    Moved client validations into concerns.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,16 @@
 module ApplicationHelper
   def set_active
-    val = case [action_name, params[:controller]]
-          when %w(index clients)
-            %w(active inactive inactive inactive)
-          when %w(show todolists)
-            %w(inactive active inactive inactive)
-          when %w(new clients)
-            %w(inactive inactive active inactive)
-          when %w(show commissions)
-            %w(inactive inactive inactive active)
-          else
-            %w(active inactive inactive inactive)
-          end
-    return val
+    case [action_name, params[:controller]]
+    when %w(index clients)
+      %w(active inactive inactive inactive)
+    when %w(show todolists)
+      %w(inactive active inactive inactive)
+    when %w(new clients)
+      %w(inactive inactive active inactive)
+    when %w(show commissions)
+      %w(inactive inactive inactive active)
+    else
+      %w(active inactive inactive inactive)
+    end
   end
 end
---
+++ app/models/concerns/client_validations.rb
@@ -0,0 +1,14 @@
+module ClientValidations
+  extend ActiveSupport::Concern
+
+  included do
+    validates :name, :guests_of_honor, :type_of_event, :minimum_guarantee,
+              :amount_of_guests, :base_price, :deposit, presence: true
+    validates :minimum_guarantee, :amount_of_guests, :phone_number,
+              :base_price, :additional_charges, :deposit, numericality: true
+
+    validates_date :payment_2_date, on_or_after: :payment_1_date
+    validates_date :payment_1_date, :menu, :payment_2_date, :date_of_event,
+                  :final_payment_date, on_or_after: :date_booked
+  end
+end
