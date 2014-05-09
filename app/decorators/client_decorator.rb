6a59f615d1160eb5d4f8ece153bf3237dd1dffa9
Dom Leone <ohheydom@gmail.com>
2014-05-09 13:40:28 -0400

    Added more methods to ClientsDecorator.

--- app/decorators/client_decorator.rb
+++ app/decorators/client_decorator.rb
@@ -4,4 +4,12 @@ class ClientDecorator < ApplicationDecorator
   def delete_link
     h.link_to 'Delete', object, method: :delete, data: { confirm: 'Are you sure?' }
   end
+
+  def edit_link
+    h.link_to object.name, h.edit_client_path(object)
+  end
+
+  def base_price
+    h.number_to_currency object.base_price
+  end
 end
