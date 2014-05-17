697defbd22eda8d9980f667cbae3f7a33eee2210
Dom Leone <ohheydom@gmail.com>
2014-05-17 01:02:24 -0400

    Moved decorator into class.

--- app/controllers/commissions_controller.rb
+++ app/controllers/commissions_controller.rb
@@ -1,5 +1,7 @@
 class CommissionsController < ApplicationController
   before_filter :authenticate_user!
+
   def show
+    @commissions = CommissionCalculator.new(current_user)
   end
 end
---
+++ app/models/commission_calculator.rb
@@ -0,0 +1,21 @@
+class CommissionCalculator
+  def initialize(user)
+    @user = user
+  end
+
+  def new_events_this_month
+    CommissionDecorator.decorate_collection(@user.positive_adjustments(type: 'new_events', month: 'this_month'))
+  end
+
+  def current_events_this_month
+    CommissionDecorator.decorate_collection(@user.positive_adjustments(type: 'current', month: 'this_month'))
+  end
+
+  def new_events_last_month
+    CommissionDecorator.decorate_collection(@user.positive_adjustments(type: 'new_events', month: 'last_month'))
+  end
+
+  def current_events_last_month
+    CommissionDecorator.decorate_collection(@user.positive_adjustments(type: 'current', month: 'last_month'))
+  end
+end
