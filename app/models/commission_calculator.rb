698a41139505a106fb89e7964b3ee1782a96b42d
Dom Leone <ohheydom@gmail.com>
2014-06-12 18:50:00 -0400

    Added attr_reader to user.

--- app/models/commission_calculator.rb
+++ app/models/commission_calculator.rb
@@ -1,21 +1,23 @@
 class CommissionCalculator
+  attr_reader :user
+
   def initialize(user)
     @user = user
   end
 
   def new_events_this_month
-    @user.positive_adjustments(type: 'new_events', month: 'this_month')
+    user.positive_adjustments(type: 'new_events', month: 'this_month')
   end
 
   def current_events_this_month
-    @user.positive_adjustments(type: 'current', month: 'this_month')
+    user.positive_adjustments(type: 'current', month: 'this_month')
   end
 
   def new_events_last_month
-    @user.positive_adjustments(type: 'new_events', month: 'last_month')
+    user.positive_adjustments(type: 'new_events', month: 'last_month')
   end
 
   def current_events_last_month
-    @user.positive_adjustments(type: 'current', month: 'last_month')
+    user.positive_adjustments(type: 'current', month: 'last_month')
   end
 end
